4c9e1b7de7d11ca97cfa628cc2252d76e907c0e4
Flávio Dutra <flaviomdutra@outlook.com>
2024-02-24 22:33:40 -0300

    Add link to product details page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
 import { GetStaticProps } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import Stripe from "stripe";
 import { HomeContainer, Product } from "../styles/pages/home";
 
@@ -26,14 +27,16 @@ export default function Home({ products }: Readonly<HomeProps>) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products?.map((product) => (
-        <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        <Link href={`/product/${product.id}`} key={product.id}>
+          <Product className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
       ))}
     </HomeContainer>
   );
